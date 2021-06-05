5e6ce4e4c013875e6c20b16bd0a24971ab9bf246
keerthna-manikandan <67958625+keerthna-manikandan@users.noreply.github.com>
2021-06-05 18:19:59 +0530

    Update size.cpp

--- size.cpp
+++ size.cpp
@@ -7,4 +7,9 @@ int main()
    cout << "Size of int : " << sizeof(int) << endl;
    cout << "Size of float : " << sizeof(float) << endl;
    cout << "Size of double : " << sizeof(double) << endl;
+   
+    float A,B;
+    cout<<"\n\nEnter two numbers:";
+    cin>>A>>B;
+    cout<<"\nProduct = "<<A*B;
 }
